1447976dc5dddf6925e5178b25631ca163ca6b61
= <sjablonski.it@gmail.com>
2021-04-16 14:56:21 +0200

    Init commit

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { Button, Result as ResultComponent } from 'antd';
+import RegistrationForm from 'pages/RegistrationForm';
+import Result from 'pages/Result';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={RegistrationForm} />
+          <Route path="/result" component={Result} />
+          <Route
+            path="*"
+            render={(props) => {
+              console.log(props);
+              return (
+                <ResultComponent
+                  status="404"
+                  title="404"
+                  subTitle="Przepraszamy, odwiedzona strona nie istnieje."
+                  extra={
+                    <Link to={{ pathname: '/' }}>
+                      <Button type="primary">Powrót do strony głównej</Button>
+                    </Link>
+                  }
+                />
+              );
+            }}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Result/Result.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Result as ResultComponent } from 'antd';
+import { Redirect, useHistory } from 'react-router';
+
+const Result = ({ location }: any) => {
+  const history = useHistory();
+
+  const { status, title, subTitle } = (location && location.state) || {};
+  if (status || title || subTitle) {
+    return (
+      <ResultComponent
+        status={status}
+        title={title}
+        subTitle={subTitle}
+        extra={[
+          <Button type="primary" key="console" onClick={() => history.replace('/', {})}>
+            Powrót
+          </Button>,
+        ]}
+      />
+    );
+  } else {
+    return <Redirect to={{ pathname: '/' }} />;
+  }
+};
+
+export default Result;
